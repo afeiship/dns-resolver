f881f7d1581ca1cc2bec73227d1b507183f5dca1
afeiship <1290657123@qq.com>
2023-07-15 22:43:25 +0800

    feat: init dns-resolver

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+const DnsResolver = (): void => {
+  console.log('hello');
+};
+
+// for commonjs es5 require
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = DnsResolver;
+}
+
+export default DnsResolver;

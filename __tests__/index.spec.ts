1698936bd56c2822361ce8bf5f73406a8c514338
afeiship <1290657123@qq.com>
2023-07-15 22:52:18 +0800

    feat: done results

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,7 +1,9 @@
 import fn from '../src';
 
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('normail single value case', async () => {
+    // expect(fn({ rel: true })).toBe(undefined);
+    const res = await fn('github.github.io');
+    console.log(res);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,44 @@
-const DnsResolver = (): void => {
-  console.log('hello');
+// curl 'https://tool.lu/dns/index.html' \
+//   -H 'content-type: application/x-www-form-urlencoded' \
+//   --data-raw 'host=github.github.io&type=a' \
+//   --compressed
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+
+const dnsResolver = (inDomain) => {
+  const domain = inDomain || 'github.github.io';
+  const url = `https://tool.lu/dns/index.html`;
+  const data = `host=${domain}&type=a`;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: data,
+  };
+
+  return fetch(url, options)
+    .then((res) => res.text())
+    .then((body) => {
+      const $ = cheerio.load(body);
+      const $rows = $('#bdy .tbl tbody tr');
+      const result: any = [];
+      $rows.each((i, el) => {
+        const typeEl = $(el).find('td').eq(0);
+        const ipEl = $(el).find('td').eq(1);
+        const ttlEl = $(el).find('td').eq(2);
+        const type = typeEl.text().trim();
+        const ip = ipEl.text().trim();
+        const ttl = ttlEl.text().trim();
+        if (ip) result.push({ type, ip, ttl });
+      });
+      return result;
+    });
 };
 
 // for commonjs es5 require
 if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
-  module.exports = DnsResolver;
+  module.exports = dnsResolver;
 }
 
-export default DnsResolver;
+export default dnsResolver;
